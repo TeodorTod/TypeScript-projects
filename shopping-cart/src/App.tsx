4ac3fa9e5b46961cff45939d0e874295feab5e2f
Artos Web <teodor.at.tod@gmail.com>
2022-11-13 22:42:02 +0200

    initial project commit

---
+++ shopping-cart/src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+
+
+function App() {
+
+        return (
+            <Container>
+                Opaa
+            </Container>
+        )
+    }
+
+export default App
