e9fb12c96fcb081cb6f305190c7ea8d4e3bc8880
Artos Web <teodor.at.tod@gmail.com>
2022-11-14 12:41:26 +0200

    add sticky navbar and make the design ig the cart

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -1,14 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
-import {Container} from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
+import About from './pages/About';
+import Store from './pages/Store';
+import Navbar from './components/Navbar';
+
 
 
 function App() {
 
-        return (
-            <Container>
-                Opaa
+    return (
+        <>
+            <Navbar />
+            <Container className='mb-4'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/store' element={<Store />} />
+                    <Route path='/about' element={<About />} />
+                </Routes>
             </Container>
-        )
-    }
+        </>
+    )
+}
 
 export default App
