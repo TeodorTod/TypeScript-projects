c830f37be0e36d8e6b3e32de4b3dab14d634c0c3
Artos Web <teodor.at.tod@gmail.com>
2022-11-15 22:17:29 +0200

    add shoppingCartContext

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Store from './pages/Store';
 import Navbar from './components/Navbar';
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
 
 
 
@@ -11,14 +12,16 @@ function App() {
 
     return (
         <>
-            <Navbar />
-            <Container className='mb-4'>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/store' element={<Store />} />
-                    <Route path='/about' element={<About />} />
-                </Routes>
-            </Container>
+            <ShoppingCartProvider>
+                <Navbar />
+                <Container className='mb-4'>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/store' element={<Store />} />
+                        <Route path='/about' element={<About />} />
+                    </Routes>
+                </Container>
+            </ShoppingCartProvider>
         </>
     )
 }
--- shopping-cart/src/components/StoreItem.tsx
+++ shopping-cart/src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import formatCurrency from '../utils/formatCurrency';
 
 
@@ -10,8 +10,9 @@ type StoreItemProps = {
 }
 
 function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+    const quantity = 0;
     return (
-        <Card>
+        <Card className="h-100">
             <Card.Img
                 variant="top"
                 src={imgUrl}
@@ -23,6 +24,28 @@ function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                     <span className="fs-2">{name}</span>
                     <span className="ms-2 text-muted">{formatCurrency(price)}</span>
                 </Card.Title>
+                <div className="mt-auto">
+                    {quantity === 0 ? (
+                        <Button className="w-100">
+                            +Add To Cart
+                        </Button>
+                    ) : <div
+                        className="d-flex align-items-center flex-column"
+                        style={{ gap: ".5rem" }}
+                    >
+                        <div
+                            className="d-flex align-items-center justify-content-center"
+                            style={{ gap: ".5rem" }}
+                        >
+                            <Button>-</Button>
+                            <div>
+                                <span className="fs-3">{quantity}</span> in cart
+                            </div>
+                            <Button>+</Button>
+                        </div>
+                        <Button variant="danger" size="sm">Remove</Button>
+                    </div>}
+                </div>
             </Card.Body>
         </Card>
     )
