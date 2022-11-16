3304c81d5bb431e8c9d01386aa0bbfb0646e8a2b
Artos Web <teodor.at.tod@gmail.com>
2022-11-16 13:44:40 +0200

    add total amount to the cart

--- shopping-cart/src/components/ShoppingCart.tsx
+++ shopping-cart/src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { NavItem, Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import formatCurrency from "../utils/formatCurrency";
 import { CartItem } from "./CartItem";
+import storeItems from "../data/items.json"
 
 type ShoppingCartProps = {
     isOpen: boolean,
@@ -19,6 +21,15 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
                     {cartItems.map(item => (
                         <CartItem key={item.id} {...item} />
                     ))}
+                    <div className="ms-auto fw-bold fs-5">
+                        Total{" "}
+                        {formatCurrency(
+                            cartItems.reduce((total, cartItem) => {
+                                const item = storeItems.find(i => i.id === cartItem.id)
+                                return total + (item?.price || 0) * cartItem.quantity
+                            }, 0)
+                        )}
+                    </div>
                 </Stack>
             </Offcanvas.Body>
         </Offcanvas>
