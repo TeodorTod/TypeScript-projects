b23889a8428faa6d2d2363f235347acbf5a19ebe
Teodor Todorov <teodor.at.tod@gmail.com>
2022-12-17 23:13:37 +0200

    add navbar

---
+++ angular-demo/src/app/Container/container.component.html
@@ -0,0 +1,1 @@
+<h1>First component</h1>
--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
