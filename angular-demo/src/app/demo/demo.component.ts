873fa0ff8b5896e159a294ff0f85900117e00aa1
Teodor Todorov <teodor.at.tod@gmail.com>
2022-12-27 00:02:48 +0200

    add all lifecycle hooks

--- angular-demo/src/app/demo/demo.component.ts
+++ angular-demo/src/app/demo/demo.component.ts
@@ -1,11 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentInit, Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent {
+export class DemoComponent implements OnInit, OnChanges, DoCheck, AfterContentInit {
   @Input() value = '';
+
+  constructor() {
+    console.log('constructor called');
+    
+  }
+
+  ngOnChanges(change: SimpleChanges) {
+    console.log('onChanges log');
+    console.log(change);
+    
+    
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+    
+  }
+
+  ngDoCheck(): void {
+    console.log('doCheck calls');
+    
+  }
+
+  ngAfterContentInit(): void {
+    console.log('afterContentInit calls');
+    
+  }
  
 }
