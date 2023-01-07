df358558267e13d7d8375483e803f2006d4a19c9
Teodor Todorov <teodor.at.tod@gmail.com>
2023-01-07 22:31:17 +0200

    Navigate between Routes Programmatically

--- angular-demo/src/app/about/about.component.ts
+++ angular-demo/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  constructor(private route: Router) {
+
+  }
+
+  navigateToHome() {  
+    // this.route.navigate(['home'])
+    this.route.navigateByUrl('home')
+  }
 }
