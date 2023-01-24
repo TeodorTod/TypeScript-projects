ecd256bff4d7514cc7dcb3ec0ffde71a790e17e7
Teodor Todorov <teodor.at.tod@gmail.com>
2023-01-24 22:45:17 +0200

    Grouping of Form Controls

--- angular-demo/src/app/contact/contact.component.ts
+++ angular-demo/src/app/contact/contact.component.ts
@@ -7,23 +7,25 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit  {
+export class ContactComponent implements OnInit {
 
   reactiveForm: FormGroup;
 
   defaultGender = 'Male';
 
   gender = [
-    {id: '1', value: 'Male'},
-    {id: '2', value: 'Female'},
-    {id: '3', value: 'Other'}
+    { id: '1', value: 'Male' },
+    { id: '2', value: 'Female' },
+    { id: '3', value: 'Other' }
   ]
 
   ngOnInit() {
     this.reactiveForm = new FormGroup({
-      firstname: new FormControl(null, Validators.required),
-      lastname: new FormControl(null, Validators.required),
-      email: new FormControl(null, Validators.email),
+      personalDetails: new FormGroup({
+        firstname: new FormControl(null, Validators.required),
+        lastname: new FormControl(null, Validators.required),
+        email: new FormControl(null, Validators.email),
+      }),
       country: new FormControl('bulgaria'),
       gender: new FormControl('male'),
       hobbies: new FormControl(null),
@@ -32,7 +34,7 @@ export class ContactComponent implements OnInit  {
 
   onSubmit() {
     console.log(this.reactiveForm);
-    
+
   }
 
 }
