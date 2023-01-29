4daf4cd6e848fbe589f0d1bad8eaa37d7c4d1c2f
Teodor Todorov <teodor.at.tod@gmail.com>
2023-01-29 22:53:21 +0200

    add interfaces

--- angular-test/src/app/services/user.service.ts
+++ angular-test/src/app/services/user.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+  }
+
+  getUser(): Observable<User> {
+    return this.http.get<User>('https://jsonplaceholder.typicode.com/users/1')
+  }
 }
