cc6168213d87c36567aad0a7706d9a6ebf44ff87
Teodor Todorov <teodor.at.tod@gmail.com>
2023-02-07 22:36:41 +0200

    add observable

--- angular-demo/src/app/home/home.component.ts
+++ angular-demo/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { CoursesService } from '../Services/courses.service';
 
 @Component({
@@ -8,12 +9,26 @@ import { CoursesService } from '../Services/courses.service';
 })
 export class HomeComponent implements OnInit {
 
+  myObservable = new Observable((observer) => {
+    console.log('Observable starts');
+    observer.next('1');
+    observer.next('2');
+    observer.next('3');
+    observer.next('4');
+    observer.next('5');
+    
+  });
+
   constructor(private coursesService: CoursesService) { }
 
   courses: any = [];
 
   ngOnInit(): void {
     this.courses = this.coursesService.courses;
+    this.myObservable.subscribe((val) => {
+      console.log(val);
+      
+    });
   }
 
 }
